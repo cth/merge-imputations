4673ae0ad9bc9cb7c56e44eecd0e696ead23dc9e
cth <christiantheilhave@gmail.com>
2016-11-22 15:55:36 +0100

    more scores

--- src/ImputationQualityScores.jl
+++ src/ImputationQualityScores.jl
@@ -12,12 +12,6 @@ module ImputationQualityScores
 		p_bb * 2 + p_ab
 	end
 
-	type TrinomialDistribution
-		p11
-		p12
-		p22
-	end
-
 	likelihoods_to_dosage{T<:AbstractFloat}(triple::Tuple{T,T,T}) = likelihoods_to_dosage(triple[1],triple[2],triple[3])
 
 	# Note that there is a one-to-infinite correspondence between doages and genotype likelihoods.
@@ -58,25 +52,35 @@ module ImputationQualityScores
 		rSqHat = (sumXbar2/n-(meanX^2))/(sumX2/n-(meanX^2))
 	end
 
+	# check probs
+	function check_probs(pr)
+		if !isprobvec(pr)
+			throw("$pr is not probability distribution")
+		end
+		pr
+	end
+
+
+	# To simulate probabilities 
+	probs(pr::Tuple{Float64,Float64,Float64}) = check_probs([pr...])
+	probs(pr::Array{Float64,1}) = check_probs(pr)
+	probs(pr::Array{Tuple{Float64,Float64,Float64},1}) = foldl(+, [0,0,0], pr)
 
 	# Comparisons of known and imputed genotypes
 	"The bhattacharya_coefficient measures overlap between to trinomial probability distributions"
-	# Can we support different types using a @generated function?
-	#macro etype(x)
-	#	if typeof(x) :< Tuple{Float64,Float64,Float64}	
-	#		[x...] 
-	#end
-	function bhattacharya_coefficient(
-			known::Tuple{Float64,Float64,Float64}, 
-			imputed::Tuple{Float64,Float64,Float64}) 
-		sum(sqrt.([known...].*[imputed...]))
-	end
-	function bhattacharya_coefficient(known::Multinomial, imputed::Multinomial)
+	function bhattacharya_coefficient(known, imputed)
 		@assert length(known) == length(imputed)
 		sum(sqrt.(probs(known).*probs(imputed)))
 	end
 
 	"Hellinger score is a measure of distance between to probability distributions"
-	function hellinger_score(known,imputed) = 1-sqrt(1-bhattacharya_coefficient(known,imputed))
+	hellinger_score(known,imputed) = 1-sqrt(1-bhattacharya_coefficient(known,imputed))
+
+	"`sen_score` (Scaled Euclidian Norm) score measures distance between genotype-wise expectations of two distributions. Note, can only distinguish between dose differences, and not genotype likelihood differences."
+	function sen_score(known,imputed)
+		m_observed = 2-(probs(known)[2] + 2*probs(known[1])) 
+		m_imputed = 2-(probs(imputed)[2] + 2*probs(imputed[1])) 
+		1 - (((m_observed-m_imputed)^2)/4)
+	end
 
 end # module
