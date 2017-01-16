d240536f07caba5b4f4281ee44b5e1efa5535824
Christian Theil Have <christiantheilhave@gmail.com>
2017-01-16 10:10:38 +0100

    calculation of INFO added

--- merge_imputed_vcf.jl
+++ merge_imputed_vcf.jl
@@ -1,54 +1,126 @@
 #!/home/fng514/bin/julia
 #$ -S /home/fng514/bin/julia
 #$ -cwd 
-# Christian Theil Have, 2016.
+# Christian Theil Have, 2017.
+# Merging of imputed data. 
+# 
+# Merges a selected set of individuals from a pair of imputations on the same panel.
+# If the same individual is present in more both imputations, then the script will
+# select the best-possible imputed genotypes for the individuals from either imputation 
+# on a per genotype basis. INFO scores as well as AN (allele number) and (AC) allele 
+# count is recalculated and the output VCF file is written with the same fields.
+# 
+# It is not as generic as I would like it to be: 
+# Currently, designed to work with the format produced by Sangers imputation server.
+# Adaptation will be need for other formats.
 
 using ArgParse
 
-function process_info(old_info1, old_info2, calculated_info_score)
-	old_info1
+######## Calculation of INFO scores ########
+tiny_constant = 10.0^-30
+check_probs(pr) = pr
+probs(pr::Tuple{Float64,Float64,Float64}) = check_probs([pr...])
+probs(pr::Array{Float64,1}) = check_probs(pr)
+probs(pr::Array{Tuple{Float64,Float64,Float64},1}) = normalize([foldr((x,y)->(x[1]+y[1],x[2]+y[2], x[3]+y[3]), (.0,.0,.0), pr)...],1)
+freqA(g) = 0.5*probs(g)[2] + probs(g)[1]
+expected_dosage_variance(g,gprobs) = 4*gprobs[1] + gprobs[2] - (2*freqA(gprobs))^2
+observed_dosage_variance(g,gprobs) = sum(map(x->(2*probs(x)[1] + probs(x)[2])^2,g) - (2*freqA(gprobs))^2)  / length(g)
+function rsquared_hat(likelihoods::Array{Tuple{Float64,Float64,Float64},1}) 
+	gprobs = probs(likelihoods)
+	expected = expected_dosage_variance(likelihoods,gprobs)
+	observed = observed_dosage_variance(likelihoods,gprobs)
+	if expected == observed == 0.0
+		1.0
+	else
+		observed / (expected+tiny_constant)
+	end
 end
+
+@everywhere function process_info(old_info1, old_info2,  allele_number, allele_count,calc_info_score)
+	field1 = split(old_info1,';')[1] # We expect first field is RefPanelAF
+	string("$(field1);AN=$(allele_number);AC=$(allele_count);INFO=$(round(calc_info_score,3))")
+end
+
 
 @everywhere function process_genotypes( fields1, fields2, keep, dict1, dict2)
 	gpidx = first(find(x -> x=="GP", split(fields1[9],':')))
+	gtidx = first(find(x -> x=="GT", split(fields1[9],':')))
 
-	maxgp(x) = foldl(max,map(x->parse(Float64,x), split(split(x,':')[gpidx],',')))
+	println("processing genotype $(fields1[3])")
 
+	genotype_likelihood(x) = (map(x->parse(Float64,x), split(split(x,':')[gpidx],','))...)
+
+	maxgp(x) = foldl(max,x)
+
+	# FIXME: This function is depends on phased genotypes
+	function count_alleles(x) 
+		tmp=split(x,':')[gtidx]
+		if tmp == "0|0"
+			0
+		elseif tmp == "0|1" || tmp == "1|0"
+			1
+		elseif tmp == "1|1"
+			2
+		else
+			throw("Unknown allele $(tmp)")
+		end
+	end
+	
+	
 	best_genotypes = Array{String,1}()
+	genotype_likelihoods = Array{Tuple{Float64,Float64,Float64},1}()
 
+	allele_number = length(keep) * 2  
+	allele_count = 0
+
+	indv_processed = 0 
 	for indv in keep
+		if indv_processed % 1000 == 0
+			write(STDERR,".")
+		end
 		max_gp_value = 0.0
 		genotype_field = nothing
+		max_gl = nothing 
+
 		try 
-			tmp_max_gp = maxgp(fields1[dict1[indv]])
-			if max_gp_value < maxgp(fields1[dict1[indv]])
+			gl_indv1 = genotype_likelihood(fields1[dict1[indv]])
+			tmp_max_gp = foldl(max,gl_indv1)
+			if max_gp_value < tmp_max_gp
 				max_gp_value = tmp_max_gp 
 				genotype_field = fields1[dict1[indv]]
+				max_gl = gl_indv1
 			end
 		end
 
 		try 
-			tmp_max_gp = maxgp(fields2[dict2[indv]])
-			if max_gp_value < maxgp(fields2[dict2[indv]])
+			gl_indv2 = genotype_likelihood(fields2[dict2[indv]])
+			tmp_max_gp = foldl(max,gl_indv2)
+			if max_gp_value < tmp_max_gp
 				max_gp_value = tmp_max_gp 
 				genotype_field = fields2[dict2[indv]]
+				max_gl = gl_indv2
 			end
 		end
 
 		if genotype_field != nothing
-			push!( best_genotypes, genotype_field)
+			push!(best_genotypes, genotype_field)
+			push!(genotype_likelihoods, max_gl)
+			allele_count = allele_count + count_alleles(genotype_field) 
+		else
+			write(STDERR, "Could not find individual $(indv)\n")
 		end
+		indv_processed =  indv_processed + 1
 	end
+
+	write(STDERR,'\n')
 
 	@assert length(keep) == length(best_genotypes)
 
-	best_genotypes
+	(best_genotypes,process_info(fields1[8], fields2[8], allele_number, allele_count, rsquared_hat(genotype_likelihoods)))
 end
 
-# FIXME: Stub for now 
-@everywhere calculate_info_score(genotypes) = 1.0
 
-# We know that the files have exactly the same number of header lines
+# We know that the files has exactly the same number of header lines
 @everywhere function process_line(line1, line2, keep=nothing, dict1=nothing, dict2=nothing)
 	if (ismatch(r"^#CHROM", line1) && ismatch(r"^#CHROM", line2))
 		fields1 = split(chomp(line1))
@@ -58,15 +130,15 @@ end
 			keep = unique(vcat(fields1[10:end],fields2[10:end]))
 		else
 			keep = intersect( keep, unique(vcat(fields1[10:end],fields2[10:end])))
+			#println(keep)
 		end
+		#println(keep)
 
 		write(STDERR, "Keeping $(length(keep)) individuals\n") 
 
 		# reverse lookup positions
-		#dict1 = Dict(fields[i] => i for i in fields1)
-		#dict1 = Dict(fields[i] => i for i in fields2)
- 		dict1 = [ fields1[i] => i for i in 1:length(fields1) ]
-		dict2 = [ fields2[i] => i for i in 1:length(fields2) ]
+		dict1 = Dict(fields1[i] => i for i in 1:length(fields1))
+		dict2 = Dict(fields2[i] => i for i in 1:length(fields2))
 
 		return (join(foldl(vcat,[fields1[1:9],keep,['\n']]),'\t'),keep,dict1,dict2)
 
@@ -83,9 +155,7 @@ end
 		# INFO fields fields1[8] != fields2[8] 
 		@assert fields1[9] == fields2[9]
 
-		new_genotypes = process_genotypes(fields1, fields2, keep, dict1, dict2)
-		new_info = process_info(fields1[8], fields2[8], calculate_info_score(new_genotypes))
-
+		(new_genotypes,new_info) = process_genotypes(fields1, fields2, keep, dict1, dict2)
 		return (join(foldl(vcat,[fields1[1:7],[new_info],new_genotypes,['\n']]),'\t'),keep,dict1,dict2)
 	end
 end
@@ -101,6 +171,7 @@ function main(args)
 		"vcf2"
 		"--keep" 
 		help = "A file with a list of ids to keep. One id per line."
+		"--out"
 	end
 
 	parsed_args = parse_args(s) # the result is a Dict{String,Any}
@@ -109,16 +180,20 @@ function main(args)
 		println("  $key  =>  $(repr(val))")
 	end
 
-	println(dump(parsed_args))
-
 	if parsed_args["keep"] != nothing
-		keep = map(chomp, readstrings(parsed_args["keep"]))
+		keep = Array{String,1}()
+		open(parsed_args["keep"]) do keepfile
+			for line in eachline(keepfile)
+				push!(keep,chomp(line))
+			end
+		end
 	else
 		keep = nothing
 	end
 
 	vcf1 = open(parsed_args["vcf1"])
 	vcf2 = open(parsed_args["vcf2"])
+	out = open(parsed_args["out"],"w")
 
 	lines1 = eachline(vcf1)
 	lines2 = eachline(vcf2)
@@ -134,13 +209,14 @@ function main(args)
 		(current_line2, state2) = next(lines2, state2)
 
 		(processed_line, keep, keep_positions1, keep_positions2) =  process_line(current_line1,current_line2, keep, keep_positions1,keep_positions2) 
-
-		print(processed_line)
+		write(out,processed_line)
 	end
 
 	close(vcf1)
 	close(vcf2)
+	close(out)
 end
 
 main(ARGS)
 
+# TEST: Merging the a vcf with itself should yield itself 
